7c2796aea2084008af2edd6cd997b8691c7d55d5
Ririko Nakamura <raindellty@live.cn>
2021-01-18 16:56:04 +0800

    Commit for sync

--- idol/scripts/skill.js
+++ idol/scripts/skill.js
@@ -1,7 +1,12 @@
 class skill {
 
-    constructor() {
+    constructor(obj) {
+        this.name = obj.name
+        this.name_jp = obj.name_jp
+    }
 
+    render() {
+        // draw item box
     }
 
 }
@@ -9,6 +14,20 @@ class skill {
 class liveSkill extends skill {
 
     constructor() {
+        this.
+
+    }
+
+    render() {
+        
+        this.element = skill.render();
+        var skillIcon = document.createElement("image");
+        this.element.appendChild(skillIcon);
+        var skillName = document.createElement("label");
+        if (language == "zh_CN")
+            skillName.innerText = this.name;
+        else if (language == "ja_JP")
+            skillName.innerText = this.name_jp;
 
     }
 
